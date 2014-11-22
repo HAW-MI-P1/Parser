7931226ca61a8d47242daee73a603fd88a11e901
nora <bla@blub.de>
2014-11-22 18:07:54 +0100

    more tests

--- src/com/mip/nora_lukas/parser/nlp/ParserTest.java
+++ src/com/mip/nora_lukas/parser/nlp/ParserTest.java
@@ -39,6 +39,20 @@ public class ParserTest {
 		String[] objects03={"dog","pig","cat"};
 		assertTrue(test03.containsValue(Arrays.asList(objects03)));
 		
+		JSONObject test04=parser.parse("who is called Jane?");
+		System.out.println(test04);
+
+		
+		String attribute04_key="attribute";
+		String attribute04_value="name";
+
+		String value04_key="value";
+		String[] value04_value={"Jane"};
+		
+		assertTrue(test04.get(attribute04_key).equals(attribute04_value));
+		assertTrue(test04.get(value04_key).equals(Arrays.asList(value04_value)));
+
+		
 	}
 	
 	@Ignore
