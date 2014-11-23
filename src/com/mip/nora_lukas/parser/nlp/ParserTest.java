92dd53af03ab5ccb6d8091b10a2f68d75a00692d
nora <bla@blub.de>
2014-11-23 15:43:35 +0100

    more tests and interface matching json

--- src/com/mip/nora_lukas/parser/nlp/ParserTest.java
+++ src/com/mip/nora_lukas/parser/nlp/ParserTest.java
@@ -8,6 +8,7 @@ import jdk.nashorn.internal.ir.annotations.Ignore;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -16,29 +17,29 @@ import java.util.HashMap;
 import java.util.List;
 public class ParserTest {
 
-//THIS IS A TEST
+	//THIS IS A TEST
 	Parser parser;
-    @Before
-    public void setUp() {
-        System.out.println("Starting Parser Test");
-        parser=new Parser();
-    }
+	@Before
+	public void setUp() {
+		System.out.println("Starting Parser Test");
+		parser=new Parser();
+	}
 
-	
+
 	@Test
 	public void testSimpleSentences() {
 		JSONObject test01=parser.parse("who likes dogs?");
 		String[] objects01={"dog"};
 		assertTrue(test01.containsValue(Arrays.asList(objects01)));
-		
+
 		JSONObject test02=parser.parse("which person likes dogs and cats?");
 		String[] objects02={"dog","cat"};
 		assertTrue(test02.containsValue(Arrays.asList(objects02)));
-		
+
 		JSONObject test03=parser.parse("who likes (dogs and cats) or pigs?");
 		String[] objects03={"dog","pig","cat"};
 		assertTrue(test03.containsValue(Arrays.asList(objects03)));
-		
+
 		JSONObject test04=parser.parse("who is called Jane?");
 		System.out.println(test04);
 		String attribute04_key="attribute";
@@ -48,9 +49,23 @@ public class ParserTest {
 		assertTrue(test04.get(attribute04_key).equals(attribute04_value));
 		assertTrue(test04.get(value04_key).equals(Arrays.asList(value04_value)));
 
-		
+		/*
+		 * more dimensions:
+		 */
+		JSONObject test05=parser.parse("who is called Jane and likes cats?");
+		System.out.println(test05);
+		String attribute05_key="attribute";
+		String attribute05_value="name";
+		String value05_key="value";
+		String[] value05_value={"Jane"};
+		//System.out.println("test05.get( \"and\")"+test05.get("and"));
+		ArrayList<JSONObject> list05 =(ArrayList<JSONObject>) test05.get("and");
+		assertTrue(list05.get(0).get(attribute05_key).equals(attribute05_value));
+		assertTrue(list05.get(0).get(value05_key).equals(Arrays.asList(value05_value)));
+
+
 	}
-	
+
 	@Ignore
 	@Test
 	public void testMoreComplexSentences() {
--- src/com/mip/nora_lukas/parser/nlp/SimpleTest.java
+++ src/com/mip/nora_lukas/parser/nlp/SimpleTest.java
@@ -8,14 +8,15 @@ public class SimpleTest {
 	 * @param args
 	 */
 
-	
-	
+
+
 	public static void main(String[] args) {
 		Parser parser=new Parser();
 		JSONObject json=parser.parse("Who (in the age between 20 and 30) is in South Afrika(10) right now and likes cats and dogs or pigs?");
-		System.out.println("json:"+json);
-		//parser.parse("Who (in the age between 20 and 30) has cats?");
+
+		//	JSONObject json= parser.parse("Who is a cat?");
+		System.out.println("json: "+json);
 	}
-	
+
 
 }
